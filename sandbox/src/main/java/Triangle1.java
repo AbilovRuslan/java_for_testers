0f1718a2881e3cc5d095baf52d03491e7c6e2675
Abilov RU <ru_a@mail.ru>
2024-08-04 22:47:50 +0300

    Add сonditions and tests for a triangle with a negative side

--- sandbox/src/main/java/Triangle1.java
+++ sandbox/src/main/java/Triangle1.java
@@ -1,5 +1,11 @@
-public record Triangle1(double a, double b, double c) {
+public record Triangle1(double a, double b, double c)  {
 
+    public Triangle1 {
+        // Условие с отрицательной стороной
+        if (a < 0 || b < 0 || c < 0) {
+            throw new IllegalArgumentException("The length of a triangle's side cannot be negative or zero.");
+        }
+    }
 
     public static void main(String[] args) {
         Triangle1 triangle1 = new Triangle1(5.0, 5.0, 8.0);
@@ -7,6 +13,8 @@ public record Triangle1(double a, double b, double c) {
         triangle1.calculateArea();
 
     }
+
+
 
     // вычисления периметра
     public double calculatePerimeter() {
--- sandbox/src/test/java/triangleTest1.java
+++ sandbox/src/test/java/triangleTest1.java
@@ -1,20 +1,34 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class triangleTest1 {
 
     @Test
     void testCalculatePerimeter() {
         Triangle1 triangle = new Triangle1(5.0, 5.0, 8.0);
         double result = triangle.calculatePerimeter();
-        Assertions.assertEquals(18.0, result, "Периметр треугольника должен быть 18.0");
+        assertEquals(18.0, result, "Периметр треугольника должен быть 18.0");
     }
 
     @Test
     void testCalculateArea() {
         Triangle1 triangle = new Triangle1(5.0, 5.0, 8.0);
         double result = triangle.calculateArea();
-        Assertions.assertEquals(12.0, result, "Площадь треугольника должна быть 12.0");
+        assertEquals(12.0, result, "Площадь треугольника должна быть 12.0");
     }
 
+
+    @Test
+    public void testTriangleWithNegativeSide() {
+        try {
+            new Triangle1(-5.0, 5.0, 8.0);
+            // Если исключение не было выброшено, данный код не должен выполняться
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
+    }
 }
+
