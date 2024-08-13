6890d14004ca33d0118dca6a227572fce70b4a21
Abilov RU <ru_a@mail.ru>
2024-08-13 15:10:26 +0300

    Add 'Test base', ApplicationManager,Group & login Helper

---
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupRemovalTests extends TestBase {
+
+
+    @Test
+    public void canRemoveGroup() {
+        //если нет групп, создаем группу, а потом по скрипту сносим
+        if (!app.isGroupPresent()) {
+            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+        }
+
+        app.groups().removeGroup();
+
+    }
+
+
+}
