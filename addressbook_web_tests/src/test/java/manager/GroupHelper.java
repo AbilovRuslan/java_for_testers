5ff917246b948125d26070200cbc2d2e1a6b1cdc
Abilov RU <ru_a@mail.ru>
2024-08-20 13:14:12 +0300

    Лекция 4.2. Количество элементов в коллекции (group creation+removal+modification added get count)

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -68,13 +68,14 @@ public class GroupHelper extends HelperBase {
         type(By.name("group_header"), group.header());
         type(By.name("group_footer"), group.footer());
     }
-
-    public boolean isGroupPresent() {
-        openGroupsPage();
-        return isElementPresent(By.name("selected[]"));
-    }
+    
 
     private void returnToGroupsPage() {
         click(By.linkText("group page"));
+    }
+
+    public int getCount() {
+        openGroupsPage();
+        return manager.driver.findElements(By.name("selected[]")).size();
     }
 }
