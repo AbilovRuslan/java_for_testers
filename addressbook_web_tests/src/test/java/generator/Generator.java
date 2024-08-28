a454677b4a7ff03e789b782daa69958c68d68463
Abilov RU <ru_a@mail.ru>
2024-08-28 22:25:37 +0300

    group json +

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -3,57 +3,72 @@ package generator;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import common.CommonFunctions;
 import model.ContactData;
 import model.GroupData;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Generator {
 
-    @Parameter(names = {"--type", "-t"})
-    String type;
+        @Parameter(names = {"--type", "-t"}, variableArity = true)
+        List<String> types; // Список типов (например, contacts, groups)
 
-    @Parameter(names = {"--output", "-o"})
-    String output;
+        @Parameter(names = {"--output", "-o"})
+        String output;
 
-    @Parameter(names = {"--format", "-f"})
-    String format;
+        @Parameter(names = {"--format", "-f"}, variableArity = true)
+        List<String> formats; // Список форматов
 
-    @Parameter(names = {"--count", "-c"})
-    int count;
+        @Parameter(names = {"--count", "-c"})
+        int count;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var generator = new Generator();
         JCommander.newBuilder()
                 .addObject(generator)
                 .build()
                 .parse(args);
         generator.run();
-
     }
 
-    private void run() {
-        var data = generate();
-        save(data);
-    }
+    private void run() throws IOException {
+        // Если types не был задан, выбрасываем ошибку
+        if (types == null || types.isEmpty()) {
+            throw new IllegalArgumentException("Необходимо указать хотя бы один тип.");
+        }
 
-    private void save(Object data) {
-    }
-
-    private Object generate() {
-        if ("contacts".equals(type)) {
-            return generateContact();
-        } else if ("groups".equals(type)) {
-            return generateGroups();
-        } else {
-            throw new IllegalArgumentException("Неизвестное что-то" + type);
+        // Проверяем каждый элемент списка types
+        for (String type : types) {
+            if ("contacts".equals(type)) {
+                var data = generateContact(); // Генерируем контакты
+                save(data); // Сохраняем контакты
+            } else if ("groups".equals(type)) {
+                var data = generateGroups(); // Генерируем группы
+                save(data); // Сохраняем группы
+            } else {
+                throw new IllegalArgumentException("Неизвестный тип: " + type);
+            }
         }
     }
 
-
+    private void save(Object data) throws IOException {
+        for (String format : formats) {
+            if ("json".equals(format)) {
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.enable(SerializationFeature.INDENT_OUTPUT);
+                mapper.writeValue(new File(output), data);
+            } else {
+                throw new IllegalArgumentException("Неизвестный формат: " + format);
+            }
+        }
+    }
 
     private Object generateContact() {
         var result = new ArrayList<ContactData>();
@@ -66,8 +81,6 @@ public class Generator {
         return result;
     }
 
-
-
     private Object generateGroups() {
         var result = new ArrayList<GroupData>();
         for (int i = 0; i < count; i++) {
@@ -78,6 +91,4 @@ public class Generator {
         }
         return result;
     }
-
 }
-
